dc696e9bface9da7db9780d769bad50be8c015d3
angel¿ <155915830+NopAngel@users.noreply.github.com>
2026-05-24 20:06:55 -0400

    `NGGAS`

---
+++ setup.sh
@@ -0,0 +1,21 @@
+# LOLOLOLOLOLOLOLo
+
+printf "Setup - NopAngel\nDotfiles <LOL>\nThis is my config, btw"
+# sudo pacman -S fastfetch gcc btop zsh wlogout alacritty cava rofi nvim vi vim git feh lua polybar make binutils tmux i3 picom qemu emacs-nox
+#sudo pacman -Syu
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+mkdir .config/nvim
+git clone --depth 1 https://github.com/NopAngel/qiwivim ~/.config/nvim
+#mkdir dotfiles
+#cd dotfiles
+#sleep 5
+clear
+fastfetch
+#alacritty
+printf "LISTO! está instalada mi Dotfile (weno, las cosas importantes :D)"
+sleep 3
+nvim
+
+
+# ADASKSAJKFASDJFKASDFASDJKAFJFakfjKFjkfsdfjlahfjafhadfdsjl
